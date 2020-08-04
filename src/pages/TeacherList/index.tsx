fc8feca410068bb55819dcb30c8a6292c53aaf82
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-04 02:08:17 -0300

    Adicionando as informações dos professores no TeacherList

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
 import './styles.css';
 
 function TeacherList() {
@@ -25,6 +27,38 @@ function TeacherList() {
           </div>
         </form>
       </PageHeader>
+
+      <main>
+        <article className="teacher-item">
+          <header>
+            <img 
+              src="https://avatars2.githubusercontent.com/u/47956746?s=460&u=109cb6b8cc13e2b97c080ce08a16d3c5ea6708c5&v=4" 
+              alt="Rian Marlon"
+            />
+            <div>
+              <strong>Rian Marlon</strong>
+              <span>Física</span>
+            </div>
+          </header>
+
+          <p>
+            Entusiasta das melhores técnicas de Física do mundo.
+            <br /><br />
+            Apaixonado por fazer cálculos de explodir o cerébro. Físicos do mundo todo o reconhecem como o Isaac Newtow II.
+          </p>
+
+          <footer>
+            <p>
+              Preço/Hora
+              <strong>R$250,00</strong>
+            </p>
+            <button>
+              <img src={whatsappIcon} alt="WhatsApp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+      </main>
     </div>
   );
 }
