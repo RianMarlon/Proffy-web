1bb267f7eadb95d7bceb758140c6f0b0d1e68eb7
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-06 10:03:58 -0300

    Adicionando a propriedade description no componente PageHeader

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 
 function TeacherForm() {
-    return (
-        <div id="page-teacher-form" className="content">
-            <PageHeader title="Que incrível que voce quer dar aulas." />
-        </div>
-    );
+  return (
+    <div id="page-teacher-form" className="content">
+      <PageHeader 
+        title="Que incrível que voce quer dar aulas." 
+        description="O primeiro passo é preencher esse formulário de inscrição."  
+      />
+    </div>
+  );
 }
 
 export default TeacherForm;
