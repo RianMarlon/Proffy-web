48daf9fa25ef4148f909f308bbeae2258f7fb479
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-06 19:56:15 -0300

    Adicionando valor default no select do componente Select

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -40,8 +40,8 @@ const Select: React.FC<SelectProps> = ({ name, label, sort, options, ...rest }) 
   return (
     <div className="select-block">
       <label htmlFor={name}>{label}</label>
-      <select id={name} {...rest}>
-        <option value="" disabled selected hidden>Selecione uma opção...</option>
+      <select id={name} {...rest} defaultValue={""}>
+        <option value="" disabled hidden>Selecione uma opção</option>
 
         {options.map((option: OptionItem) => {
           return (
