a94ab2c7c5b0429cc9dd52d1af08a10683660402
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-06 14:46:16 -0300

    Adicionando o componente Textarea e o componente Select no TeacherForm

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
 
@@ -20,12 +22,32 @@ function TeacherForm() {
           <legend>Seus dados</legend>
           <Input name="name" label="Nome completo" type="name" />
           <Input name="avatar" label="Avatar" type="url" />
-          <Input name="whatsapp" label="WhatsApp" type="text" />
+          <Input name="whatsapp" label="WhatsApp" />
+          <Textarea name="biography" label="Biografia" />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
-          <Input name="subject" label="Matéria" type="text" />
+          <Select 
+            name="subject" 
+            label="Matéria"
+            options={[
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Química', label: 'Quimíca' },
+              { value: 'Português', label: 'Português' },
+              { value: 'Redação', label: 'Redação' },
+              { value: 'História', label: 'História' },
+              { value: 'Filosofia', label: 'Filosofia' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'Sociologia', label: 'Sociologia' },
+              { value: 'Inglês', label: 'Inglês' },
+              { value: 'Espanhol', label: 'Espanhol' },
+              { value: 'Educação Física', label: 'Educação Física' },
+              { value: 'Artes', label: 'Artes' }
+            ]}
+          />
           <Input name="cost" label="Custo da sua hora por aula" type="url" />
         </fieldset>
 
