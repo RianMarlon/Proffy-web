d8a7a015c366df9a2a0290c22e5a22db4128e2ae
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-07 14:30:10 -0300

    Alterando o componente TeacherItem para receber as propriedades necessárias

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -3,36 +3,55 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem() {
+export interface Teacher {
+  id: number,
+  avatar: string,
+  biography: string,
+  cost: number,
+  name: string,
+  subject: string,
+  whatsapp: string
+}
+
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('/connections', {
+      user_id: teacher.id
+    });
+  }
   return (
     <article className="teacher-item">
       <header>
         <img 
-          src="https://avatars2.githubusercontent.com/u/47956746?s=460&u=109cb6b8cc13e2b97c080ce08a16d3c5ea6708c5&v=4" 
-          alt="Rian Marlon"
+          src={teacher.avatar}
+          alt={teacher.name}
         />
         <div>
-          <strong>Rian Marlon</strong>
-          <span>Física</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
 
       <p>
-        Entusiasta das melhores técnicas de Física do mundo.
-        <br /><br />
-        Apaixonado por fazer cálculos de explodir o cérebro. Físicos do mundo todo o reconhecem como o Isaac Newtow II.
+        {teacher.biography}
       </p>
 
       <footer>
         <p>
           Preço/Hora
-          <strong>R$250,00</strong>
+          <strong>R${teacher.cost}</strong>
         </p>
-        <button>
+        <a onClick={createNewConnection}
+          type="button" href={`https://wa.me/${teacher.whatsapp}`} target="__blank">
           <img src={whatsappIcon} alt="WhatsApp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
