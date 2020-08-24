0413c5c210895d7249e091aa11a4601058e54ab5
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-24 01:35:10 -0300

    Adicionando o TeachersProvider no componente Routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,13 +3,16 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
+import { TeachersProvider } from './contexts/TeachersContext';
 
 function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={Landing} />
-            <Route path="/study" component={TeacherList} />
-            <Route path="/give-classes" component={TeacherForm} />
+            <TeachersProvider>
+                <Route path="/" exact component={Landing} />
+                <Route path="/study" component={TeacherList} />
+                <Route path="/give-classes" component={TeacherForm} />
+            </TeachersProvider>
         </BrowserRouter>
     );
 }
