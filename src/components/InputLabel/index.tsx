1c6228b1637c709bf2ddc7b8219c40cce361c977
Rian Marlon <rianmarlon20@yahoo.com>
2020-08-30 18:23:48 -0300

    Adicionando a estrutura do componente InputLabel da pasta components

---
+++ src/components/InputLabel/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, ReactNode } from 'react';
+import { InputProps } from '../Input';
+
+import './styles.css';
+
+interface InputLabelProps extends InputProps {
+  buttonRight?: ReactNode
+}
+
+const InputLabel: React.FC<InputLabelProps> = ({ name, label, type, value, onChange, buttonRight }) => {
+  const [classNameInput, setClassNameInput] = useState('');
+  const classNameInputBlock = `input-label-block ${buttonRight && 'has-button-right'}`;
+
+  useEffect(() => {
+    if (value && value.toString().trim()) {
+      setClassNameInput('focus');
+    }
+
+    else {
+      setClassNameInput('no-focus');
+    }
+  }, [value]);
+
+  return (
+    <div className={classNameInputBlock}>
+      <input className={classNameInput} 
+        type={type} id={name} value={value} onChange={onChange} />
+      <label htmlFor={name}>{label}</label>
+      { buttonRight && (
+        <div className="button-right">
+          {buttonRight}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default InputLabel;
