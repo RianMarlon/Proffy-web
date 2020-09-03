d1eac7c1ef679f49e70e28204f7272aa976d7093
Rian Marlon <rianmarlon20@yahoo.com>
2020-09-02 21:26:53 -0300

    Adicionando a rota para a página Cadastre

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,18 +5,20 @@ import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
 import { TeachersProvider } from './contexts/TeachersContext';
 import Login from './pages/Login';
+import Cadastre from './pages/Cadastre';
 
 function Routes() {
-    return (
-        <BrowserRouter>
-            <TeachersProvider>
-                <Route path="/" exact component={Login} />
-                <Route path="/home" component={Landing} />
-                <Route path="/study" component={TeacherList} />
-                <Route path="/give-classes" component={TeacherForm} />
-            </TeachersProvider>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <TeachersProvider>
+        <Route path="/" exact component={Login} />
+        <Route path="/cadastre" component={Cadastre} />
+        <Route path="/home" component={Landing} />
+        <Route path="/study" component={TeacherList} />
+        <Route path="/give-classes" component={TeacherForm} />
+      </TeachersProvider>
+    </BrowserRouter>
+  );
 }
 
 export default Routes;
