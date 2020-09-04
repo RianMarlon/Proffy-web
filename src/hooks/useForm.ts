026a919be1181215bc9f2a56158c024ef189fd7e
Rian Marlon <rianmarlon20@yahoo.com>
2020-09-04 17:08:41 -0300

    Adicionando o hook useForm na pasta hooks

---
+++ src/hooks/useForm.ts
@@ -0,0 +1,48 @@
+import { useState, ChangeEvent } from 'react';
+
+function useForm(initialState: any) {
+  const [form, setForm] = useState(initialState);
+  const [errors, setErrors] = useState<any>({});
+
+  function updateField(e: ChangeEvent<HTMLInputElement>) {
+    const newForm = {...form};
+    const newErrors = { ...errors };
+
+    newForm[e.target.name] = e.target.value;
+    newErrors[e.target.name] = false;
+
+    setForm({...newForm});
+    setErrors({...newErrors});
+  }
+
+  function validateFields() {
+    const newErrors: any = {};
+    
+    Object.entries(form).forEach(([field, value]) => {
+      const isEmpty = typeof value === 'string' ? !value.trim() : !value;
+      newErrors[field] = isEmpty;
+    });
+    
+    setErrors({
+      ...newErrors
+    });
+  }
+  
+  function hasOneFieldEmpty() {
+    const hasFieldEmpty = (value: any) => !value.trim();
+    return Object.values(form).some(hasFieldEmpty);
+  }
+
+  function hasOneError() {
+    const hasError = (value: any) => !value;
+    return Object.values(errors).some(hasError);
+  }
+
+  return [
+    form, errors,
+    updateField, validateFields,
+    hasOneFieldEmpty, hasOneError
+  ]
+}
+
+export default useForm;
