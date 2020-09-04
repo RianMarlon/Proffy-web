ce4cece5aad77dadd0dd68440c8cae6af3f45fe1
Rian Marlon <rianmarlon20@yahoo.com>
2020-09-04 17:22:34 -0300

    Adicionando os atributos labelError e error no InputProps do componente input da pasta components

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -5,13 +5,20 @@ import './styles.css';
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string,
   label: string,
+  labelError?: string,
+  error?: boolean,
 }
 
-const Input: React.FC<InputProps> = ({ name, label, ...rest }) => {
+const Input: React.FC<InputProps> = ({ label, labelError, error, ...rest }) => {
+  const classNameLabel = error ? 'error' : '';
+
   return (
     <div className="input-block">
-      <label htmlFor={name}>{label}</label>
-      <input type="text" id={name} {...rest} />
+      <label className={classNameLabel} 
+        htmlFor={rest.name}>
+          {error ? labelError : label}
+      </label>
+      <input {...rest} />
     </div>
   );
 }
