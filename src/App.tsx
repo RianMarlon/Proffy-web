0c4ca22955e32b30d9702cea96a57ece8260e137
Rian Marlon <rianmarlon20@yahoo.com>
2020-09-12 16:17:16 -0300

    Adicionando o css do React Toastify no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
-import './assets/styles/global.css';
 import Routes from './routes';
+
+import 'react-toastify/dist/ReactToastify.min.css';
+import './assets/styles/global.css';
 
 function App() {
   return (
