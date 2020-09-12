ab70e43417f6387930f2cecbd1603d445befea58
Rian Marlon <rianmarlon20@yahoo.com>
2020-09-12 16:30:21 -0300

    Adicionando o React Toastify e o componente Sucess na página Register da pasta pages

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,20 +1,20 @@
 import React, { useState, useEffect, FormEvent } from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
 
 import useForm from '../../hooks/useForm';
 import api from '../../services/api';
 
-import Proffy from '../../components/Proffy';
 import InputLabel from '../../components/InputLabel';
 import InputPasword from '../../components/InputPassword';
+import Proffy from '../../components/Proffy';
+import Success from '../../components/Success';
 
 import backPurpleIcon from '../../assets/images/icons/back-purple.svg';
 
 import './styles.css';
 
 function Register() {
-
-  const history = useHistory();
 
   const initialFields = {
     firstName: '',
@@ -34,6 +34,8 @@ function Register() {
   const [differentPasswords, setDifferentPasswords] = useState(false);
   const [buttonSubmitDisabled, setButtonSubmitDisabled] = useState(true);
   const regexValidateEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+  const [isSuccess, setIsSuccess] = useState(false);
 
   useEffect(() => {
     const hasValidEmail = regexValidateEmail.test(form.email);
@@ -77,90 +79,107 @@ function Register() {
 
     api.post('/signup', data)
       .then(() => {
-        alert('Cadastro realizado com sucesso!');
-        history.push('/');
+        setIsSuccess(true);
       })
-      .catch(() => {
-        alert('Erro ao realizar o cadastro!');
+      .catch(({ response }) => {
+        const messageError = response.data.error;
+        toast.error(messageError, {
+          autoClose: 3000
+        });
       });
   }
 
   return (
-    <div className="cadastre-container">
-      <header className="proffy-block-right">
-        <Proffy />
-      </header>
-      <main className="cadastre-content">
-        <div>
-          <div className="header">
-            <Link to="/">
-              <img src={backPurpleIcon} alt="Voltar"/>
-            </Link>
+    <>
+      {
+        !isSuccess ? (
+          <div className="cadastre-container">
+            <ToastContainer />
+            <header className="proffy-block-right">
+              <Proffy />
+            </header>
+            <main className="cadastre-content">
+              <div>
+                <div className="header">
+                  <Link to="/">
+                    <img src={backPurpleIcon} alt="Voltar"/>
+                  </Link>
+                </div>
+                <div className="body">
+                  <div className="form-container">
+                    <h1>Cadastro</h1>
+                    <p>Preencha todos os dados abaixo para começar</p>
+                    <form onSubmit={handleSubmitCadastre}>
+                      <InputLabel
+                        name="firstName"
+                        type="text"
+                        label="Nome"
+                        labelError="Nome não informado"
+                        error={errors.firstName}
+                        value={form.firstName}
+                        onChange={updateField}
+                        required={true}
+                      />
+                      <InputLabel
+                        name="lastName"
+                        type="text"
+                        label="Sobrenome"
+                        labelError="Sobrenome não informado"
+                        error={errors.lastName}
+                        value={form.lastName}
+                        onChange={updateField}
+                        required={true}
+                      />
+                      <InputLabel
+                        name="email"
+                        type="email"
+                        label="E-mail"
+                        labelError="E-mail não informado"
+                        error={errors.email}
+                        value={form.email}
+                        onChange={updateField}
+                        required={true}
+                      />
+                      <InputPasword
+                        name="password"
+                        label="Senha"
+                        labelError="Senha não informada"
+                        error={errors.password}
+                        value={form.password}
+                        onChange={updateField}
+                        required={true}
+                      />
+                      <InputPasword
+                        name="confirmPassword"
+                        label="Confirme sua senha"
+                        labelError={labelTextError}
+                        error={errors.confirmPassword || differentPasswords}
+                        value={form.confirmPassword}
+                        onChange={updateField}
+                        required={true}
+                      />
+      
+                      <button className="button-submit" type="submit"
+                        disabled={buttonSubmitDisabled}>
+                        Concluir cadastro
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </main>
           </div>
-          <div className="body">
-            <div className="form-container">
-              <h1>Cadastro</h1>
-              <p>Preencha todos os dados abaixo para começar</p>
-              <form onSubmit={handleSubmitCadastre}>
-                <InputLabel
-                  name="firstName"
-                  type="text"
-                  label="Nome"
-                  labelError="Nome não informado"
-                  error={errors.firstName}
-                  value={form.firstName}
-                  onChange={updateField}
-                  required={true}
-                />
-                <InputLabel
-                  name="lastName"
-                  type="text"
-                  label="Sobrenome"
-                  labelError="Sobrenome não informado"
-                  error={errors.lastName}
-                  value={form.lastName}
-                  onChange={updateField}
-                  required={true}
-                />
-                <InputLabel
-                  name="email"
-                  type="email"
-                  label="E-mail"
-                  labelError="E-mail não informado"
-                  error={errors.email}
-                  value={form.email}
-                  onChange={updateField}
-                  required={true}
-                />
-                <InputPasword
-                  name="password"
-                  label="Senha"
-                  labelError="Senha não informada"
-                  error={errors.password}
-                  value={form.password}
-                  onChange={updateField}
-                  required={true}
-                />
-                <InputPasword
-                  name="confirmPassword"
-                  label="Confirme sua senha"
-                  labelError={labelTextError}
-                  error={errors.confirmPassword || differentPasswords}
-                  value={form.confirmPassword}
-                  onChange={updateField}
-                  required={true}
-                />
-
-                <button className="button-submit" type="submit"
-                  disabled={buttonSubmitDisabled}>
-                  Concluir cadastro
-                </button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
+        ) : (
+          <Success 
+            title="Cadastro concluído!"
+            description="Agora você faz parte da plataforma da Proffy.
+            Tenha uma ótima experiência."
+            textButton="Fazer login"
+            routeButton="/"
+          />
+        )
+      }
+    </>
   );
 }
 
