fba70bec78fd1f679cafd8ef6766bf48368fb9a9
Rian Marlon <rianmarlon20@yahoo.com>
2020-09-12 16:11:16 -0300

    Adicionando o arquivo auth.ts com funções de autenticação e retorno de token na pasta services

---
+++ src/services/auth.ts
@@ -0,0 +1,80 @@
+import api from './api';
+
+export const TOKEN_KEY = '@proffy/user';
+
+export const hasToken = () => {
+
+  const tokenLocalStorage = localStorage.getItem(TOKEN_KEY);
+  const tokenSessionStorage = sessionStorage.getItem(TOKEN_KEY);
+  
+  const hasTokenInLocalStorage = tokenLocalStorage !== null;
+  const hasTokenInSessionStorage = tokenSessionStorage !== null;
+
+  if (hasTokenInLocalStorage || hasTokenInSessionStorage) {
+    return true;
+  }
+
+  else {
+    return false;
+  }
+};
+
+export const hasTokenValid = async () => {
+
+  if(hasToken()) {
+    const tokenLocalStorage = localStorage.getItem(TOKEN_KEY);
+    const tokenSessionStorage = sessionStorage.getItem(TOKEN_KEY);
+
+    const hasTokenInLocalStorage = tokenLocalStorage !== null;
+    const hasTokenInSessionStorage = tokenSessionStorage !== null;
+
+    if (hasTokenInLocalStorage) {
+      const data = {
+        token: tokenLocalStorage
+      }
+
+      const response = await api.post('/validate-token', data);
+      const { isTokenValid } = response.data;
+
+      if (!isTokenValid) {
+        localStorage.removeItem(TOKEN_KEY);
+      }
+      return isTokenValid;
+    }
+
+    if (hasTokenInSessionStorage) {
+      const data = {
+        token: tokenSessionStorage
+      }
+
+      const response = await api.post("/validate-token", data);
+      const { isTokenValid } = response.data;
+      
+      if (!isTokenValid) {
+        sessionStorage.removeItem(TOKEN_KEY);
+      }
+
+      return isTokenValid;
+    }
+  }
+
+  else {
+    return false;
+  }
+};
+
+export const getToken = () => {
+  const tokenLocalStorage = localStorage.getItem(TOKEN_KEY);
+  const tokenSessionStorage = sessionStorage.getItem(TOKEN_KEY);
+  
+  const hasTokenInLocalStorage = tokenLocalStorage !== null;
+  const hasTokenInSessionStorage = tokenSessionStorage !== null;
+
+  if (hasTokenInLocalStorage) {
+    return tokenLocalStorage;
+  }
+
+  if (hasTokenInSessionStorage) {
+    return tokenSessionStorage;
+  }
+};
