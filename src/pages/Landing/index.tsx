47d109cd0be0ce5f700beec90854c8477942fa10
Rian Marlon <rianmarlon20@yahoo.com>
2020-09-19 18:54:04 -0300

    Adicionando o componente HeaderProfile na página Landing da pasta pages

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -5,27 +5,44 @@ import api from '../../services/api';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
-
 import studyIcon from '../../assets/images/icons/study.svg';
+
+import HeaderProfile from '../../components/HeaderProfile';
+
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './styles.css';
-
 function Landing() {
   const [totalConnections, setTotalConnections] = useState(0);
+  const [me, setMe] = useState({
+    first_name: '',
+    avatar: '',
+    email: '',
+  });
 
   useEffect(() => {
     api.get('/connections')
       .then(response => {
         const { total } = response.data;
         setTotalConnections(total);
-      })
+      });
+
+    api.get('/me')
+      .then(response => {
+        const { user } = response.data;
+        setMe({ ...user });
+      });
   }, []);
 
   return (
     <div id="page-landing">
-      <div id="page-landing-content" className="container">
+      <HeaderProfile 
+        name={me.first_name}
+        image={me.avatar}
+        email={me.email}
+      />
+      <main id="page-landing-content" className="container">
         <div className="logo-container">
           <img src={logoImg} alt="Proffy"/>
           <h2>Sua plataforma de estudos online.</h2>
@@ -53,7 +70,7 @@ function Landing() {
           Total de {totalConnections} conexões já realizadas
           <img src={purpleHeartIcon} alt="Coração roxo"></img>
         </span>
-      </div>
+      </main>
     </div>
   );
 }
