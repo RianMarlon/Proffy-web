ed2eaacf93d9535cc38278ead65e092c3fe8ca76
Rian Marlon <rianmarlon20@yahoo.com>
2020-10-14 01:06:42 -0300

    Adicionando a página Loading e o AuthContext no routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Route, Redirect, useHistory, Switch } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Route, Redirect, Switch, BrowserRouter } from 'react-router-dom';
 
 import { TeachersProvider } from './contexts/TeachersContext';
-import { hasToken, hasTokenValid } from './services/auth';
+import AuthContext from './contexts/AuthContext';
 
 import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
+import Profile from './pages/Profile';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import ForgotPassword from './pages/ForgotPassword';
 import ChangePassword from './pages/ChangePassword';
-import Profile from './pages/Profile';
+import Loading from './pages/Loading';
 
 const PrivateRoute = ({component, ...rest}: any) => {
-  const history = useHistory();
+  const { isValidToken, loading } = useContext(AuthContext);
 
-  hasTokenValid().then((response) => {
-    if (!response) history.push('/');
-  });
+  if (loading) {
+    return <Loading />;
+  }
   
   const routeComponent = (props: any) => {
-    return hasToken()
+    return isValidToken
       ? React.createElement(component, props)
       : <Redirect to={{pathname: '/', state: { from: props.location }}} />
   }
@@ -30,8 +31,14 @@ const PrivateRoute = ({component, ...rest}: any) => {
 };
 
 const PublicRoute = ({component, ...rest}: any) => {
+  const { isValidToken, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <Loading />;
+  }
+
   const routeComponent = (props: any) => (
-    !hasToken()
+    !isValidToken
       ? React.createElement(component, props)
       : <Redirect to={{pathname: '/home', state: { from: props.location }}} />
   );
